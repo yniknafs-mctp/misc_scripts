987d13d51714be8193b3d4a78e31f168d7dc5973
yniknafs <yniknafs@pathbio-dx11.path.med.umich.edu>
2014-10-22 16:57:39 -0400

    tester

---
+++ source/test/test2.py
@@ -0,0 +1,31 @@
+'''
+Created on Oct 22, 2014
+@author yniknafs
+'''
+
+
+import os
+import sys
+import argparse
+import logging
+import collections
+
+
+
+
+
+    
+def main():
+    # parse command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG,
+                      format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    
+    
+    return 0
+
+if __name__ == '__main__': 
+    sys.exit(main())
